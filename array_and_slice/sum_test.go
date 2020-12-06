0b06ee4b63bef14ba098207d2412e7cb8909ddd6
PHChenGit <chenpohsun1@gmail.com>
2020-12-06 14:31:14 +0800

    [Test]: Add a test case which is a collection with any size

--- array_and_slice/sum_test.go
+++ array_and_slice/sum_test.go
@@ -3,12 +3,25 @@ package array_and_slice
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	excepted := 15
+		got := Sum(numbers)
+		excepted := 15
 
-	if got != excepted {
-		t.Errorf("got %d, excepted %d", got, excepted)
-	}
+		if got != excepted {
+			t.Errorf("got %d, excepted %d", got, excepted)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := [3]int{1, 2, 3}
+
+		got := Sum(numbers)
+		excepted := 6
+
+		if got != excepted {
+			t.Errorf("got %d, excepted %d", got, excepted)
+		}
+	})
 }
