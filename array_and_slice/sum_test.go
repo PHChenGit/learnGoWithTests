92ca7a720b28098b17ce48ccf8686c32764aaff6
PHChenGit <chenpohsun1@gmail.com>
2020-12-06 14:20:20 +0800

    [Test]: Add logic unit test

---
+++ array_and_slice/sum_test.go
@@ -0,0 +1,14 @@
+package array_and_slice
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	numbers := [5]int{1, 2, 3, 4, 5}
+
+	got := Sum(numbers)
+	excepted := 15
+
+	if got != excepted {
+		t.Errorf("got %q, excepted %q", got, excepted)
+	}
+}
